ff74223144fa7d4f71faa4924525774b183e2479
satish24011993 <satishkurakula073@gmail.com>
2020-09-04 20:52:40 +0530

    Notifications Added

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -525,7 +525,7 @@ def send_student_notification(request):
         "to":token
     }
     headers={"Content-Type":"application/json","Authorization":"key=AAAAyJjPPkk:APA91bEhqTkVSSlKmxETU4_6tGUe4a_czCFE5xZ8t1D3p_SaoZ8KQGSCZhiXVinmv6x01biCG41J4XrDj7Zt_3XqdkLpLyS_ZLvq_Y-s8M-BQIzGLG174EfFR-OVPFfOVLHML5jF5nbY"}
-    data = request.post(url,data=json.dumps(body),headers=headers)
+    data = request.POST(url,data=json.dumps(body),headers=headers)
     notification = NotificationStudent(student_id = student, message=message)
     notification.save()
     print(data.text)
